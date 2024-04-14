e02ce0a7f5563313e57eaf91ae59d918f82f800c
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2024-04-14 18:13:03 +1000

    Make theater refuelling flight plan on-station time configurable based on the desired mission duration

--- game/ato/flightplans/theaterrefueling.py
+++ game/ato/flightplans/theaterrefueling.py
@@ -17,7 +17,7 @@ class TheaterRefuelingFlightPlan(RefuelingFlightPlan):
 
     @property
     def patrol_duration(self) -> timedelta:
-        return timedelta(hours=1)
+        return self.flight.coalition.game.settings.desired_player_mission_duration + timedelta(minutes=30)
 
 
 class Builder(IBuilder[TheaterRefuelingFlightPlan, PatrollingLayout]):
